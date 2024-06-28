fc6881a74127dec9d189da384cfdc8c902bee7a1
TKelia <k.tesi@alustudent.com>
2024-06-28 04:17:05 -0700

    extended Python script to export data in the CSV format

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,27 +1,56 @@
 #!/usr/bin/python3
-"""Exports to-do list info for a given employee ID to CSV format."""
+"""
+fetching user information and export TODO list to a CSV file
+"""
 import csv
 import requests
-import sys
+from sys import argv
+
+
+def get_employee_info(employee_id):
+    """
+    Get employee information by employee ID
+    """
+    url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
+    response = requests.get(url)
+    return response.json()
+
+
+def get_employee_todos(employee_id):
+    """
+    Get the TODO list of the employee by employee ID
+    """
+    url = f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos'
+    response = requests.get(url)
+    return response.json()
+
+
+def export_to_csv(employee_id, username, todos):
+    """
+    Export TODO list to a CSV file
+    """
+    filename = f'{employee_id}.csv'
+    with open(filename, mode='w') as file:
+        file_writer = csv.writer(file, delimiter=',', quoting=csv.QUOTE_ALL)
+        for todo in todos:
+            rowData = [employee_id, username, todo['completed'], todo['title']]
+            file_writer.writerow(rowData)
+
+
+def main(employee_id):
+    """
+    Main function to fetch user info and TODO list, then export to CSV
+    """
+    user = get_employee_info(employee_id)
+    username = user.get("username")
+
+    todos = get_employee_todos(employee_id)
+
+    export_to_csv(employee_id, username, todos)
+
 
 if __name__ == "__main__":
-    employee_id=sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
-    todo ="https://jsponplaceholder.typicode.com/todo?userId={}"
-    todo = todo.format(employee_id)
-
-    user_info = requests.request("GET",url).json()
-    todo_info = requests.request("GET",todo).json()
-
-    employee_name = user_info.get("name")
-    total_tasks = list(filter(lambda x:(x["completed"] is True),todo_info))
-    task_com = len(total_tasks)
-    total_task_done = len(todo_info)
-
-    with open(str(employee_id) + '.csv' , "w") as f:
-        [f.writer('"' + str(employee_id) + '",'+
-                  '"' + employee_username + '",'+
-                  '"' + str(task["completed"]) + '",'+
-                  '"' + task("title") + '",'+ "\n")
-            
-          for task in todo_info]
+    if len(argv) > 1:
+        main(argv[1])
+    else:
+        print("Usage: ./1-export_to_CSV.py <employee_id>")
